e16ac10b87378f9f8f3605db6c83e821da2fe540
GudrunInga <gudrunib09@ru.is>
2016-01-27 14:08:22 +0000

    End of first session, comments are the way towards power

--- src/test/java/org/ggp/base/player/gamer/statemachine/sample/TestSinglePlayer.java
+++ src/test/java/org/ggp/base/player/gamer/statemachine/sample/TestSinglePlayer.java
@@ -3,11 +3,16 @@ package org.ggp.base.player.gamer.statemachine.sample;
 import org.junit.Assert;
 import org.junit.Test;
 
+/*
+ * Við viljum hann keyri gefinn leik sem við þekkjum lausnirnar á.
+ * Athuga hvort hann sé að gefa upp réttar lausnir.
+ */
 public class TestSinglePlayer extends Assert {
 
+	// For unit testing player.gamer.statemachine.sample.SinglePlayer.java
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSingleGamer() {
+
 	}
 
 }
