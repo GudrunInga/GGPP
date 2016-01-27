010fcf8d00682b3b33d6ef5712bfdadc560b5883
GudrunInga <gudrunib09@ru.is>
2016-01-27 12:38:50 +0000

    singlePlayer class added

---
+++ src/main/java/org/ggp/base/player/gamer/statemachine/sample/SinglePlayer.java
@@ -0,0 +1,18 @@
+package org.ggp.base.player.gamer.statemachine.sample;
+
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class SinglePlayer extends SampleGamer {
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
