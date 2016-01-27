b47efa3136740574a7d545326fab038ecd688e2b
GudrunInga <gudrunib09@ru.is>
2016-01-27 13:50:26 +0000

    Unit test class added

---
+++ src/test/java/org/ggp/base/player/gamer/statemachine/sample/TestSinglePlayer.java
@@ -0,0 +1,13 @@
+package org.ggp.base.player.gamer.statemachine.sample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSinglePlayer extends Assert {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
