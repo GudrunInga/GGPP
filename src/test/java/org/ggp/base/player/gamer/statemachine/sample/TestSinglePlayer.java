4ec3adbee799c5756de36ef1baad225c0a9862e9
GudrunInga <gudrunib09@ru.is>
2016-02-11 10:33:38 +0000

    unit test

--- src/test/java/org/ggp/base/player/gamer/statemachine/sample/TestSinglePlayer.java
+++ src/test/java/org/ggp/base/player/gamer/statemachine/sample/TestSinglePlayer.java
@@ -1,5 +1,10 @@
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import org.ggp.base.player.gamer.statemachine.SinglePlayer;
+import org.ggp.base.player.gamer.statemachine.random.RandomGamer;
+import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.match.Match;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,10 +13,49 @@ import org.junit.Test;
  * Athuga hvort hann sé að gefa upp réttar lausnir.
  */
 public class TestSinglePlayer extends Assert {
-
-	// For unit testing player.gamer.statemachine.sample.SinglePlayer.java
 	@Test
-	public void testSingleGamer() {
+	public void testCoins() {
+		try {
+			SinglePlayer s = new SinglePlayer();
+			Match m = new Match("", -1, 1000, 1000, GameRepository
+					.getDefaultRepository().getGame("coins"), "");
+
+			s.setMatch(m);
+			s.setRoleName(GdlPool.getConstant("you"));
+			s.metaGame(1000);
+			assertEquals("SinglePlayer", s.getName());
+
+			assertTrue(s.selectMove(1000) != null);
+			assertTrue(GdlPool.getConstant("you") == s.getRoleName());
+			if(m.isCompleted())
+			{
+				assertTrue(s.bestValue == 100);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testTicTacToe(){
+		SinglePlayer s = new SinglePlayer();
+		RandomGamer r = new RandomGamer();
+		Match m = new Match("", 10, 120, 120, GameRepository.getDefaultRepository().getGame("ticTacToe"),"");
+		s.setMatch(m);
+		r.setMatch(m);
+
+		s.setRoleName(GdlPool.getConstant("xplayer"));
+		r.setRoleName(GdlPool.getConstant("oplayer"));
+
+		assertTrue(s.getRoleName().toString() == "xplayer");
+		assertTrue(r.getRoleName().toString() == "oplayer");
+
+		if(m.isCompleted())
+		{
+			assertTrue(s.bestValue >= 50);
+
+
+		}
 
 	}
 
