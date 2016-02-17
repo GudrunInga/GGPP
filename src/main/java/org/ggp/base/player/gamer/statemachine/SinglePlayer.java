43576b77077f3925aca83d054cc1fdd9b0d69982
GudrunInga <gudrunib09@ru.is>
2016-02-17 22:19:39 +0000

    Single player still overflow

--- src/main/java/org/ggp/base/player/gamer/statemachine/SinglePlayer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/SinglePlayer.java
@@ -5,6 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.gdl.grammar.GdlLiteral;
+import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.gdl.grammar.GdlRule;
+import org.ggp.base.util.gdl.grammar.GdlSentence;
+import org.ggp.base.util.gdl.grammar.GdlTerm;
+import org.ggp.base.util.prover.aima.knowledge.KnowledgeBase;
+import org.ggp.base.util.prover.aima.substituter.Substituter;
+import org.ggp.base.util.prover.aima.substitution.Substitution;
+import org.ggp.base.util.prover.aima.unifier.Unifier;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
@@ -12,6 +22,8 @@ import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+import com.google.common.collect.Sets;
 
 public class SinglePlayer extends SampleGamer {
 
@@ -22,7 +34,9 @@ public class SinglePlayer extends SampleGamer {
 // Check if your implementation is correct by running the tests.
 
 	public StateMachine stateMachine;
-	public ArrayList<Integer> visitedState = new ArrayList<Integer>(); // States that have already been visited, don't need to check those again
+	//public ArrayList<MachineState> visitedState = new ArrayList<MachineState>(); // States that have already been visited, don't need to check those again
+	public HashMap<Integer , MachineState> visitedState = new HashMap<Integer, MachineState>();
+
 	public ArrayList<Move> bestPath;
 	public HashMap<MachineState, CacheNode> cache; //minmax transposition table // change state to have softreference apache.org. Another way is to create array e.g. Pair<State, Value>[State.hashCode()%N] of size N gives us more power if to replace the new value with the old value.
 	public int bestValue = 0;
@@ -106,7 +120,7 @@ public class SinglePlayer extends SampleGamer {
 			{
 				MachineState currState = getCurrentState();
 
-				if(bestPath != null)
+				/*if(bestPath != null)
 				{
 					for(Move move : bestPath)
 					{
@@ -114,7 +128,8 @@ public class SinglePlayer extends SampleGamer {
 						nextMove.add(move);
 						currState = stateMachine.getNextState(currState, nextMove);
 					}
-				}
+				}*/
+				visitedState = new HashMap<Integer, MachineState>();
 				//System.out.println("We are in SELECTMOVE!--------------------------------------------------------------------------\n");
 				explore(currState, 0, 1, new ArrayList<Move>());
 				//System.out.println(System.currentTimeMillis()); //Output the time it took to search
@@ -312,7 +327,7 @@ public class SinglePlayer extends SampleGamer {
 	//A að keyra á rót með depth = 0, maxDepth = 0 og movesMade tómt
 	public void explore(MachineState node, int depth, int maxDepth, ArrayList<Move> movesMade)
 	{
-		System.out.println("We are at " + depth);
+
 		//System.out.println("Depth: " + depth + "MaxDepth: " + maxDepth);
 		if(System.currentTimeMillis() >= stopTime)
 		{
@@ -323,12 +338,15 @@ public class SinglePlayer extends SampleGamer {
 		if(isChecked(node, depth))
 		{
 			//System.out.println("Already Checked");
+
 			return;
 		}
 		if(stateMachine.isTerminal(node))
 		{
 			//System.out.println("Found Terminal");
+			//System.out.println("We are at " + depth);
 			evaluate(node, movesMade);
+			visitedState.put(node.hashCode(), node);// add(node.hashCode()); //.hashCode());
 			return;
 		}
 		if(isSolved())
@@ -343,9 +361,9 @@ public class SinglePlayer extends SampleGamer {
 			return;
 		}
 		//System.out.println("I should get here!!!!!");
-		visitedState.add(node.hashCode());
+		visitedState.put(node.hashCode(), node);//add(node); //.hashCode());
 		try{
-            mostmoves =  Math.max(mostmoves,stateMachine.getLegalMoves(node, getRole()).size());
+            //mostmoves =  Math.max(mostmoves,stateMachine.getLegalMoves(node, getRole()).size());
 			for(Move child : stateMachine.getLegalMoves(node, getRole()))
 			{
 				ArrayList<Move> nextMove = new ArrayList<Move>();
@@ -371,7 +389,7 @@ public class SinglePlayer extends SampleGamer {
 			//System.out.println("depth == 0");
 			maxDepth++;
 			//System.out.println("Max depth:" + maxDepth);
-			visitedState = new ArrayList<Integer>();
+			visitedState = new HashMap<Integer, MachineState>(); //new ArrayList<MachineState>();
 			ArrayList<Move> noMoves = new ArrayList<Move>();
 			explore(node, 0, maxDepth, noMoves);
 		}
@@ -382,7 +400,7 @@ public class SinglePlayer extends SampleGamer {
 		//return true if node has been visited
 		if(singlePlayerMode)
 		{
-			if(visitedState.contains(node.hashCode()))
+			if(visitedState.containsKey(node.hashCode()) && visitedState.get(node.hashCode()) == node)//contains(node)) //.hashCode()))
 			{
 				return true;
 			}
@@ -460,8 +478,6 @@ public class SinglePlayer extends SampleGamer {
         return (int)(100*size/mostmoves);
     }
 
-
-
     //rational mobility attempts to evaluate how much control we have over the game compared to our opponents (note that in multiplayer games, this
     //will be a relatively low value, so it may diminish by comparison once we find reasonable terminal states
     //this is approximated as the the ratio
@@ -485,4 +501,63 @@ public class SinglePlayer extends SampleGamer {
 		}
         return (int)(100*size/legalSize);
     }
+    //Novelty, prefer states that are different from the current state/their parent state.
+    // Changing things as much as possible can help avoid getting stuck.
+    // When you don't know what to do, at least explore new territory
+    // The goal if often very different from the initial state.
+    //Bad because changing the game state can happen if you throw away your own pieces
+
+
+
+
+
+
+    //Goal proximity is a measure of how similar a given state is to desirable terminal state.
+	//There are various ways this can be computed.
+    //One common method is to count how many propositions that are true in the current state
+    //are also true in a terminal state with adequate utility.
+    //The difficulty of implementing this method is obtaining a set of desirable terminal
+    //states with which the current state can be compared.
+    //Another alternative is to use the utility of the given state as a measure of
+    //progress toward the goal, with the idea being that the higher utility,
+    //the closer the goal. Of course, this is not always true. However,
+    //in many games the goal values are indeed monotonoic, meaning that values do increase
+    //with proximity to the goal. Moreover, it is sometimes possible to compute this by a
+    //simple examination of the game description.
+    public int goalDistance(Role role, MachineState state)
+    {
+    	List<Gdl> rules = this.getMatch().getGame().getRules();
+		KnowledgeBase kb = new KnowledgeBase(Sets.newHashSet(rules));
+		GdlTerm[] goalArguments = {getRoleName(), GdlPool.getConstant("100")};
+		GdlSentence goalSentence = GdlPool.getRelation(GdlPool.GOAL, goalArguments);
+		List<GdlRule> goalRules = kb.fetch(goalSentence);
+		int count = 0;
+		for(GdlRule rule : goalRules) {
+			Substitution s = Unifier.unify(rule.getHead(), goalSentence);
+			if (s != null) { // found rule who's head matches goal(myrole,100)
+				// use the substitution on the rule before looking at the body
+				rule = Substituter.substitute(rule, s);
+				for (GdlLiteral literal : rule.getBody()) {
+					// TODO: process the rule body
+					// For example, if the literal is something of the form (true ?x) then
+					// try to match it to the current state.
+					int l = 0;
+					for(GdlLiteral lit : state.getContents())
+					{
+						if(literal == lit)
+						{
+							l++;
+						}
+					}
+					if(l > count)
+					{
+						count = l;
+					}
+					System.out.println(literal);
+				}
+
+			}
+		}
+		return 0;
     }
+}
