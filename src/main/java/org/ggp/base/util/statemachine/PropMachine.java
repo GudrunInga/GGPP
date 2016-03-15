fd3ad019b274ef3aa21e231a2bf991349742f786
GudrunInga <gudrunib09@ru.is>
2016-03-15 18:18:02 +0000

    Added class for Propnet implementation

---
+++ src/main/java/org/ggp/base/util/statemachine/PropMachine.java
@@ -0,0 +1,57 @@
+package org.ggp.base.util.statemachine;
+
+import java.util.List;
+
+import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class PropMachine extends StateMachine{
+
+	@Override
+	public void initialize(List<Gdl> description) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getGoal(MachineState state, Role role)
+			throws GoalDefinitionException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isTerminal(MachineState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Role> getRoles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MachineState getInitialState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Move> getLegalMoves(MachineState state, Role role)
+			throws MoveDefinitionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MachineState getNextState(MachineState state, List<Move> moves)
+			throws TransitionDefinitionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
