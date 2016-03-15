4d52cfdc9d962ddfd9200a5987c47b067843bbe5
GudrunInga <gudrunib09@ru.is>
2016-03-15 18:25:00 +0000

    added to initialize method in PropMachine, check comment

--- src/main/java/org/ggp/base/util/statemachine/PropMachine.java
+++ src/main/java/org/ggp/base/util/statemachine/PropMachine.java
@@ -3,16 +3,25 @@ package org.ggp.base.util.statemachine;
 import java.util.List;
 
 import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.propnet.architecture.PropNet;
+import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class PropMachine extends StateMachine{
-
+	/** The underlying proposition network  */
+    private PropNet propNet;
 	@Override
 	public void initialize(List<Gdl> description) {
 		// TODO Auto-generated method stub
-
+		try {
+			//There is more implementation in SamplePropNetStateMachine, maybe we need that too?
+			propNet = OptimizingPropNetFactory.create(description);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
