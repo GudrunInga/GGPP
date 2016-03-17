7b309fd18606e70ce726cb4b3d565ccdceebd7c8
GudrunInga <gudrunib09@ru.is>
2016-03-17 09:42:32 +0000

    propmachine fikt

--- src/main/java/org/ggp/base/util/statemachine/PropMachine.java
+++ src/main/java/org/ggp/base/util/statemachine/PropMachine.java
@@ -1,9 +1,15 @@
 package org.ggp.base.util.statemachine;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.propnet.architecture.Component;
 import org.ggp.base.util.propnet.architecture.PropNet;
+import org.ggp.base.util.propnet.architecture.components.Proposition;
 import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
@@ -11,56 +17,136 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class PropMachine extends StateMachine{
 	/** The underlying proposition network  */
-    private PropNet propNet;
+    private PropNet propNet; //From SamplePropNetStateMachine
+    /** The topological ordering of the propositions */
+    private List<Proposition> ordering; //From SamplePropNetStateMachine
+    /** The player roles */
+    private List<Role> roles; //From SamplePropNetStateMachine
+    //For getting  base propositions: propNet.getBasePropositions()
+    //For getting input propositions: propNet.getInputPropositions()
+
+
+
 	@Override
 	public void initialize(List<Gdl> description) {
 		// TODO Auto-generated method stub
 		try {
 			//There is more implementation in SamplePropNetStateMachine, maybe we need that too?
 			propNet = OptimizingPropNetFactory.create(description);
+			roles = propNet.getRoles();
+            ordering = getOrdering();
+			propNet.renderToFile("/Desktop/render.txt");
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
+    /**
+     * Computes the goal for a role in the current state.
+     * Should return the value of the goal proposition that
+     * is true for that role. If there is not exactly one goal
+     * proposition true for that role, then you should throw a
+     * GoalDefinitionException because the goal is ill-defined.
+     */
 	@Override
 	public int getGoal(MachineState state, Role role)
 			throws GoalDefinitionException {
-		// TODO Auto-generated method stub
-		return 0;
+		Map<Role, Set<Proposition>> n = propNet.getGoalPropositions();
+		 // TODO: Compute the goal for role in state.
+		for(Proposition p : propNet.getPropositions()){
+			for(Proposition k : n.get(role))
+				if(p.equals(state)){
+
+			}
+		}
+	//Það sem við þurfum mögulega að nota:
+		propNet.getGoalPropositions();
+		//if there is more or less than one goal then:
+			//throw new GoalDefinitionException(the machinestate, role);
+
+		return -1;
 	}
 
+    /**
+     * Computes if the state is terminal. Should return the value
+     * of the terminal proposition for the state.
+     */
 	@Override
 	public boolean isTerminal(MachineState state) {
-		// TODO Auto-generated method stub
+		// TODO: Compute whether the MachineState is terminal.
 		return false;
 	}
 
 	@Override
 	public List<Role> getRoles() {
-		// TODO Auto-generated method stub
-		return null;
+		return roles;
 	}
 
+    /**
+     * Returns the initial state. The initial state can be computed
+     * by only setting the truth value of the INIT proposition to true,
+     * and then computing the resulting state.
+     */
 	@Override
 	public MachineState getInitialState() {
-		// TODO Auto-generated method stub
+		 // TODO: Compute the initial state.
 		return null;
 	}
 
+    /**
+     * Computes the legal moves for role in state.
+     */
 	@Override
 	public List<Move> getLegalMoves(MachineState state, Role role)
 			throws MoveDefinitionException {
-		// TODO Auto-generated method stub
+		// TODO: Compute legal moves.
 		return null;
 	}
 
+    /**
+     * Computes the next state given state and the list of moves.
+     */
 	@Override
 	public MachineState getNextState(MachineState state, List<Move> moves)
 			throws TransitionDefinitionException {
-		// TODO Auto-generated method stub
+        // TODO: Compute the next state.
 		return null;
 	}
+
+	 /**
+     * This should compute the topological ordering of propositions.
+     * Each component is either a proposition, logical gate, or transition.
+     * Logical gates and transitions only have propositions as inputs.
+     *
+     * The base propositions and input propositions should always be exempt
+     * from this ordering.
+     *
+     * The base propositions values are set from the MachineState that
+     * operations are performed on and the input propositions are set from
+     * the Moves that operations are performed on as well (if any).
+     *
+     * @return The order in which the truth values of propositions need to be set.
+     */
+	 //From SamplePropNetStateMachine
+    public List<Proposition> getOrdering()
+    {
+        // List to contain the topological ordering.
+        List<Proposition> order = new LinkedList<Proposition>();
+
+        // All of the components in the PropNet
+        List<Component> components = new ArrayList<Component>(propNet.getComponents());
+
+        // All of the propositions in the PropNet.
+        List<Proposition> propositions = new ArrayList<Proposition>(propNet.getPropositions());
+
+        // TODO: Compute the topological ordering.
+
+        return order;
+    }
+    //Test function, propably not supposed to be here
+    @Override
+    public PropMachine getStateMachine(){
+    		return new PropMachine();
+    }
 
 }
